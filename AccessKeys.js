fde914664ce91e5ef329f96c0e265cd707bf3fb1
RubTheBuddha <sean.majure@gmail.com>
2017-09-28 22:08:13 -0700

    2nd

---
+++ AccessKeys.js
@@ -0,0 +1,22 @@
+const twitter = {
+  consumer_key: 'UU5i9xzdnMhMEsK1DvmBzLgzI',
+  consumer_secret: 'ULVD2W8afMRASmzicE8ykwLJYalpF3Dkn1kg6h3V6ecYhbgc9A',
+  access_token_key: '600401876-ow78eMvHOekVpXS6S7VLEgmZ6A0RWzEM5zOY6PGY',
+  access_token_secret: 'KgUWLPYIDhUXJbjYAgtoZsszXZ7De8XahPTu4U5GQd4zj'
+}
+
+
+const spotify = {
+  id: "9d5022b8b2c9445f9597c765b565154a",
+  secret: "446cfd5856be477f80211593a08b8d7f"
+}
+
+const omdb = "40e9cece";
+
+module.exports = twitterKeys;
+
+module.exports = {
+  twitter,
+  spotify,
+  omdb
+}
